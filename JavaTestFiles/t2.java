c5372c239a2e5422e47818304146e00de29048a8
LordJohn985 <lordjohn985@email.com>
2021-06-10 18:31:50 -0300

    Java test Files

---
+++ JavaTestFiles/t2.java
@@ -0,0 +1,21 @@
+public class test{
+
+	public void t1(){
+	}
+
+	public void t2(){
+		int test;
+		test=0;
+	}
+
+	public void t3(int a, int b, int c, int d){
+		int test;
+		test=0;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ JavaTestFiles/t3.java
@@ -0,0 +1,25 @@
+public class test{
+
+	public void t1(){
+	}
+
+	public void t2(){
+		int test;
+		test=0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
+
+public class test2{
+
+	public void t3(){
+	}
+
+	public void t4(int calc){
+	}
+
+}
